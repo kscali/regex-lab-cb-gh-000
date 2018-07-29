c20ae891c0ba6d6fa20f4f4043ef1a14bcac8a40
Koy Saeteurn <kosatun@aol.com>
2018-07-29 03:49:29 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -10,9 +10,7 @@ def words_five_letters_long(text)
   text.scan(/\b\w{5}\b/)
 end
 
-def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.scan(/^[A-Z]\w(.|!|?))/)
-end
+
 
 def valid_phone_number?(phone)
 
